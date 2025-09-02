acadca8a1b3c7c0c6be3df9284baa81072f173bc
PankajMahanto <aryanpankaj78@gmail.com>
2025-09-02 19:42:42 +0600

    day1

---
+++ Day1/Class.py
@@ -0,0 +1,19 @@
+# let's make a class
+
+class Atm:
+    
+    #! function vs method
+    '''
+    l = [1,2,3]
+    len(l) #! eta ekta function
+    
+    l.append() #! eta ekta method
+    
+    function can use any one like list use and int o use kore
+    but method only particular class object can uses.
+    
+    
+    '''
+    
+    def __init__(self):
+        
---
+++ Day1/Object.py
@@ -0,0 +1,5 @@
+# Object literal
+l = list()
+print(type(l))
+s =set()
+print(type(s))
