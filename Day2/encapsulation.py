feca26d91075b8e8ca016f3f486bcf1340a573f2
PankajMahanto <aryanpankaj78@gmail.com>
2025-09-04 20:13:13 +0600

    encapsulation and instance variable

---
+++ Day2/encapsulation.py
@@ -0,0 +1,11 @@
+'''
+#! Encapsulation
+
+#! Instance Variable 
+Constructor er vitore je variable gula use kora hoy tader kei mainly
+#! instance variable bola hoy
+
+#! Ar ekta main bishoy holo je variable gula mainly
+every object er value different hoy setai holo instance variable
+
+'''
