9a2383826fb4be6cf7fde20ff6f0375d4660a3e7
PankajMahanto <aryanpankaj78@gmail.com>
2025-09-04 20:54:51 +0600

    atm

---
+++ Day2/e1.py
@@ -0,0 +1,85 @@
+class Atm:
+
+    #! Constructor
+    def __init__(self):
+        self.pin = ""
+        self.balance = 0
+
+        #! Manu method call here
+        self.manu()
+
+    def manu(self):
+        user_input = input(
+            """
+        Hello, Welcome to Our ATM system.
+            1. Create Your Pin.
+            2. Deposit Your Balance.
+            3. Withdraw Your Balance.
+            4. Check Your Current Balanced.
+            5. Exit.                 
+                    """
+        )
+
+        if user_input == "1":
+            print("Create Pin")
+            self.create_pin()
+
+        elif user_input == "2":
+            print("Deposit")
+            self.deposit()
+
+        elif user_input == "3":
+            print("Withdraw Balanced")
+            self.withdraw()
+
+        elif user_input == "4":
+            print("Check Balance")
+            self.check_balance()
+
+        else:
+            print("Exit")
+
+    def check_pin(self):
+        temp = input("Enter your pin: ")
+        if temp == self.pin:
+            print("Access Granted....")
+            return 1
+        else:
+            print("Invalid Pin Try again!!!")
+            return 0
+
+    def create_pin(self):
+        self.pin = input("Enter your pin: ")
+        print("Create Pin Successfully")
+        self.manu()
+
+    def deposit(self):
+        if self.check_pin()==1:
+            amount = int(input("Enter your Amount: "))
+            self.balance += amount
+            print("Deposit Successful")
+        else:
+            print("Try Again!!...")
+
+        self.manu()
+
+    def withdraw(self):
+        if self.check_pin()==1:
+            amount = int(input("Enter your withdraw amount: "))
+            if self.balance>=amount:
+                self.balance -= amount
+                print("Withdraw Successful.")
+            else:
+                print("Insufficient Fund!!!.....")
+        else:
+            print("Try Again!!.....")
+
+        self.manu()
+
+    def check_balance(self):
+        if self.check_pin()==1:
+            print("Current balanced: ", self.balance)
+        else:
+            print("Try Again!!!.....")
+
+        self.manu()
--- Day2/encapsulation.py
+++ Day2/encapsulation.py
@@ -8,4 +8,27 @@ Constructor er vitore je variable gula use kora hoy tader kei mainly
 #! Ar ekta main bishoy holo je variable gula mainly
 every object er value different hoy setai holo instance variable
 
+#! Encapsulation holo ekta security method or way?
+Capsule jemon 2 ta jinis dia banano hoy thik sei rokhon encapsulation
+mainly 2 ta jinis dia banano hoy.
+#! eta mainly use kora hoy securely jate Data type and method
+#! je kew jene change kore nite nah pare
+
+#! etar jonno private variable kora hoy
+#! in python does not any keyword like private which i use in JAVA
+#! programming language. We use in python in (__) any variable and method
+#! automatically those variable and method convert to Private.
+#! But in python does not actually exist PRIVATE. Because if user wish then
+use it (_ClassName__VariableName/MethodName).
+
+#! Why encapsulation use to achieve private property.
+
+so that the main code does not any change to achieve this logic we
+use #! Encapsulation ---
+#! Getter and Setter Method
+Getter method -> Get the current value of the variable which we call
+Setter method -> We change the variable value but must be follow the instruction
+
+We achieve the encapsulation getter and setter method and also secure the data type and method for that reason this approach is call Encapsulation.
+
 '''
